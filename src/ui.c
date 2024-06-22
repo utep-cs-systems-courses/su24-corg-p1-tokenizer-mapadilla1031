f9fc304fd08d615ec8b6917f4f4c65708a56aac4
mapadilla1031 <mapadilla10@miners.utep.edu>
2024-06-21 21:55:24 -0600

    imp using fgets plus ui flow

--- src/ui.c
+++ src/ui.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "tokenizer.h"
 
 void userInterface()
 {
-  int c;
+  char userInput;
+  char buf[100];
+  puts("Welcome to Project-Tokenizer-");
   while(1){
-    printf("> ");
-    while ((c = getchar()) != EOF){//read next char check EOF
-      putchar(c);
-      if (c == '\n'){//break loop when enter
-	break;
+    puts("Please type one of the following options:");
+    printf(" -s- for String, -h- for History, -x- to exit!\n");
+    printf(">");
+    scanf(" %c", &userInput);
+    while(getchar() != '\n');
+
+    switch (userInput){
+    case 's':
+      puts("Please enter a string");
+      if(fgets(buf, sizeof(buf), stdin)){
+	printf("Echo String: &s", buf);
       }
+      break;
+    case 'h':
+      puts("History option has been selected: ");
+      break;
+    case 'x':
+      puts("Program Aborted!");
+      return;
+    default:
+      printf("Input '%c', not recognized! \n");
+      break;
     }
-    if (c== EOF){
-      break;//stop first loop if EOF by user
-    }
+  
   }
 }
 
